cdd76bfe6e2e63a71e17e72ffc17ee7169f8c5bb
pin863 <chika67zz@gmail.com>
2025-07-14 20:45:51 +0900

    label,inputの属性を修正

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,26 +9,41 @@ export default function page() {
         <div className="w-1/3 text-center ">
           <h1 className="text-4xl font-bold mt-40 mb-10">Sign Up</h1>
           <div className="text-left mx-16">
-            <label className="text-xl pl-3 font-bold">Name</label>
+            <label htmlFor="username" className="text-xl pl-3 font-bold">
+              Name
+            </label>
             <input
+              id="username"
+              name="username"
               type="text"
               className="bg-gray-100 text-xl shadow-md rounded-xl py-3 pl-3 w-full mt-3 mb-5 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
               placeholder="Enter your name"
             />
-            <label className="text-xl pl-3 font-bold">Email</label>
+            <label htmlFor="email" className="text-xl pl-3 font-bold">
+              Email
+            </label>
             <input
-              type="text"
+              id="email"
+              name="email"
+              type="email"
               className="bg-gray-100 text-xl shadow-md rounded-xl py-3 pl-3 w-full mt-3 mb-5 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
               placeholder="Enter your email"
             />
-            <label className="text-xl pl-3 font-bold">Password</label>
+            <label htmlFor="password" className="text-xl pl-3 font-bold">
+              Password
+            </label>
             <input
+              id="password"
+              name="password"
               type="password"
               className="bg-gray-100 text-xl shadow-md rounded-xl py-3 pl-3 w-full mt-3 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
               placeholder="Enter your password"
             />
           </div>
-          <button className="bg-sky-500 font-bold text-white rounded-full mt-10 py-3 px-10 text-xl shadow-md hover:bg-sky-300 hover:-translate-y-1 transform  duration-300">
+          <button
+            type="submit"
+            className="bg-sky-500 font-bold text-white rounded-full mt-10 py-3 px-10 text-xl shadow-md hover:bg-sky-300 hover:-translate-y-1 transform  duration-300"
+          >
             Sign Up
           </button>
           <div className="flex justify-center mt-4 font-bold space-x-3">
