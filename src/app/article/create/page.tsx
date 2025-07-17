b24df81af0d7b2b4197a428aa382df62a3a3dda7
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-18 02:11:14 +0900

    記事投稿画面作成中_7/18_2:11

--- src/app/article/create/page.tsx
+++ src/app/article/create/page.tsx
@@ -1,9 +1,37 @@
 // 記事投稿用画面
 
+"use client";
+
+import Button from "@/app/components/button/Button";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="pt-20">
+        <div className="flex flex-col items-center gap-5">
+          {/* タイトル */}
+          <input
+            type="text"
+            className="text-5xl font-semibold"
+            placeholder="Title"
+          />
+
+          {/* 画像アップロード */}
+          <div>
+            {/* 矢印アイコン */}
+            <Button
+              variant="Blue"
+              size="lg"
+              onClick={() => alert("画像をアップロード！")}
+            >
+              Upload Image
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
