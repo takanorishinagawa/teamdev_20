179206364959fe668bbb24c2e50b92c8553c010b
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-18 02:11:41 +0900

    記事ホーム画面編集_7/18_2:11

--- src/app/article/home/page.tsx
+++ src/app/article/home/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import Button from "@/app/components/button/Button";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const ArticleHomePage = () => {
-  return <div>page</div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <h2>記事投稿ホーム画面</h2>
+
+        {/* （仮）記事投稿画面へ遷移！ */}
+        <Button
+          variant="Blue"
+          size="md"
+          onClick={() => {
+            router.push("/article/create");
+          }}
+        >
+          記事投稿
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ArticleHomePage;
