14921a40923f4052d4e0f69f9e9e71d9b9a5f1ee
Naoya Noguchi <naoya.work0213@gmail.com>
2025-07-18 03:17:04 +0900

    記事投稿画面仮UI完成_7/18_3:17

--- src/app/article/create/page.tsx
+++ src/app/article/create/page.tsx
@@ -3,30 +3,71 @@
 "use client";
 
 import Button from "@/app/components/button/Button";
+import RectButton from "@/app/components/button/RectButton";
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
   return (
     <div>
-      <div className="pt-20">
-        <div className="flex flex-col items-center gap-5">
+      <div className="p-15">
+        <div className="flex flex-col items-center gap-10">
           {/* タイトル */}
           <input
             type="text"
-            className="text-5xl font-semibold"
+            className="w-full max-w-[1100px] min-w-[600px] text-5xl font-semibold"
             placeholder="Title"
           />
 
           {/* 画像アップロード */}
-          <div>
-            {/* 矢印アイコン */}
-            <Button
-              variant="Blue"
-              size="lg"
-              onClick={() => alert("画像をアップロード！")}
-            >
-              Upload Image
-            </Button>
+          <div className="w-full max-w-[1200px] min-w-[600px] rounded-lg border-2 border-dashed border-[#7777] p-20">
+            <div className="flex flex-col items-center gap-10">
+              {/* 矢印アイコン */}
+              <Image
+                src="/icon/create/arrow.png"
+                alt="アイコン"
+                width={60}
+                height={60}
+              />
+
+              {/* アップロードボタン */}
+              <Button
+                variant="Blue"
+                size="lg"
+                onClick={() => alert("画像をアップロード！")}
+              >
+                Upload Image
+              </Button>
+            </div>
+          </div>
+
+          {/* カテゴリー */}
+          <div className="flex w-full max-w-[1200px] min-w-[600px] justify-end">
+            <div className="flex flex-col gap-3">
+              <label htmlFor="category">Category</label>
+              <select
+                name="category"
+                id="category"
+                className="w-60 rounded-md border border-[#7777] p-3"
+              >
+                <option>Value</option>
+                <option>Value2</option>
+                <option>Value3</option>
+              </select>
+            </div>
+          </div>
+
+          {/* 投稿内容 */}
+          <textarea
+            className="min-h-[300px] w-full max-w-[1200px] min-w-[600px] rounded-lg border-none bg-[#D9D9D9]/25 p-5 text-lg text-black"
+            style={{ boxShadow: "2px 2px 10px 0 rgba(0, 0, 0, 0.25)" }}
+          ></textarea>
+
+          {/* 記事作成ボタン */}
+          <div className="flex w-full max-w-[1200px] min-w-[600px] justify-end">
+            <RectButton onClick={() => alert("記事を投稿！")}>
+              Create
+            </RectButton>
           </div>
         </div>
       </div>
