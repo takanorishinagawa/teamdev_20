331f75faebf88e4004878ed49e4bea3182e92e82
BBeiko <twoge17@gmail.com>
2025-07-20 12:34:25 +1000

    CSSを修正

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import PostCard, { Post } from "@/app/components/postCard/PostCard";
 import Header from "../components/header/Header";
 
@@ -12,43 +13,57 @@ const dummyPosts: Post[] = Array.from({ length: 9 }).map((_, i) => ({
 
 const Home = () => {
   return (
-    <main className="max-w-6xl mx-auto p-4">
-      {/* ヘッダー */}
-      {/* <header className="flex justify-between items-center py-4">
-        <h1 className="text-xl font-bold">My Blog</h1>
-        <div className="space-x-2">
-          <button className="px-4 py-2 border rounded">Login</button>
-          <button className="px-4 py-2 bg-black text-white rounded">
-            Sign Up
-          </button>
-        </div>
-      </header> */}
-      <Header/>
+    <>
+      <Header />
 
-      <section className="flex justify-center my-6">
-        <div className="relative w-full md:w-2/3">
+      <main className="p-8">
+        <section className="flex justify-center my-6 gap-3">
+          {/* flex items-center */}
           {/* 検索バー */}
-          <input
-            type="text"
-            placeholder="Search..."
-            className="w-full p-3 pl-4 pr-12 border rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-          {/* 虫眼鏡アイコン */}
+          <div className="w-full max-w-md">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full p-3 border rounded-full focus:ring-blue-400"
+            />
+          </div>
+
+          {/* 検索バーのすぐ右に虫眼鏡アイコン */}
+          <span
+            className="
+                flex items-center
+                cursor-pointer 
+                transition duration-200 
+                hover:-translate-y-1
+              "
+            onClick={() => alert("検索アイコンがクリックされました！")}
+          >
+            🔍
+          </span>
+          {/* </div> */}
+        </section>
+
+        {/* 投稿カードのグリッド */}
+        <section
+          className="
+            grid 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            gap-6
+            cursor-pointer
+          "
+        >
+          {dummyPosts.map((post, index) => (
+            <PostCard key={index} post={post} />
+          ))}
+        </section>
+
+        {/* ページネーション */}
+        <div className="flex justify-center my-6">
+          <p>ページネーション</p>
         </div>
-        <span className="ml-3 inset-y-0 right-4 flex items-center text-gray-500">
-          🔍
-        </span>
-      </section>
-
-      {/* 投稿カードのグリッド */}
-      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {dummyPosts.map((post, index) => (
-          <PostCard key={index} post={post} />
-        ))}
-      </section>
-
-      {/* ページネーション */}
-    </main>
+      </main>
+    </>
   );
 };
 
