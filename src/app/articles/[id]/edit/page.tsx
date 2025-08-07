671c01b356d782af970073695cde91b6a66e310f
BBeiko <twoge17@gmail.com>
2025-08-07 15:55:53 +1000

    記事編集ページを作成

--- src/app/articles/[id]/edit/page.tsx
+++ src/app/articles/[id]/edit/page.tsx
@@ -1,1 +1,18 @@
 // 各記事編集画面用
+
+"use client";
+
+import ArticleForm from "@/app/components/article/ArticleForm";
+import React from "react";
+
+const EditArticlePage = () => {
+  return (
+    <div>
+      <h1>Edit Article</h1>
+      {/* フォームやデータ取得処理をここに追加 */}
+      <ArticleForm/>
+    </div>
+  );
+};
+
+export default EditArticlePage;
---
+++ src/app/components/article/ArticleForm.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const ArticleForm = () => {
+  return (
+    <div>
+      <h1>Article Form</h1>
+    </div>
+  );
+};
+
+export default ArticleForm;
