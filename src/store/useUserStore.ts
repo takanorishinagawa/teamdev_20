0b418858e7f6f279fe3767dba81a36e254388dfe
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-13 23:27:06 +0900

    resetUser導入_2025_0813_23-26

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -7,6 +7,7 @@ type UsersType = Database["public"]["Tables"]["users"]["Row"];
 type StateType = {
   user: UsersType;
   setUser: (payload: UsersType) => void;
+  resetUser: () => void;
 };
 
 const useUserStore = create<StateType>((set) => ({
@@ -19,6 +20,17 @@ const useUserStore = create<StateType>((set) => ({
     updated_at: "",
   },
   setUser: (payload) => set({ user: payload }),
+  resetUser: () =>
+    set({
+      user: {
+        id: "",
+        email: "",
+        name: "",
+        image_path: "",
+        created_at: "",
+        updated_at: "",
+      },
+    }),
 }));
 
 export default useUserStore;
