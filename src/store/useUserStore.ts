c9ac65ff7be00b5709c0cf586cdaad74d9b3aaa8
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-13 16:28:14 +0900

    zustand導入＆ヘッダー編集_20250813_16-28

---
+++ src/store/useUserStore.ts
@@ -0,0 +1,24 @@
+// useUserStore.ts
+import type { Database } from "@/types/database.types";
+import { create } from "zustand";
+
+type UsersType = Database["public"]["Tables"]["users"]["Row"];
+
+type StateType = {
+  user: UsersType;
+  setUser: (payload: UsersType) => void;
+};
+
+const useUserStore = create<StateType>((set) => ({
+  user: {
+    id: "",
+    email: "",
+    name: "",
+    image_path: "",
+    created_at: "",
+    updated_at: "",
+  },
+  setUser: (payload) => set({ user: payload }),
+}));
+
+export default useUserStore;
