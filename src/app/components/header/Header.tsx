24603cac2f7a650760e636b8dce43ebf5d3033c2
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-13 15:56:38 +0900

    更新_20250813_15-56

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,16 +1,55 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import type { Session } from "@supabase/supabase-js";
+
+import { createClient } from "@/utils/supabase/clients";
+
 import Button from "../button/Button";
 
 export default function Header() {
+  const supabase = createClient();
+
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data }) => setSession(data.session));
+
+    // const { data: loginData } = supabase.auth.onAuthStateChange(
+    //   (_event, newSession) => {
+    //     setSession(newSession);
+    //   },
+    // );
+
+    // // クリーンアップ
+    // return () => {
+    //   loginData.subscription.unsubscribe();
+    // };
+  }, [supabase]);
+
   return (
     <header className="w-full">
       <div className="flex h-16 items-center justify-end gap-6 bg-blue-100 pr-10 font-bold">
-        <Button href="/auth/login" variant="white" size="sm">
-          Login
-        </Button>
-
-        <Button href="/auth/signup" variant="DarkBlue" size="sm">
-          Sign Up
-        </Button>
+        {session ? (
+          <Button
+            // TODO 記事作成ページへ遷移
+            href=""
+            variant="DarkBlue"
+            size="sm"
+          >
+            Create
+          </Button>
+        ) : (
+          <>
+            <Button href="/auth/login" variant="white" size="sm">
+              Login
+            </Button>
+            <Button href="/auth/signup" variant="DarkBlue" size="sm">
+              Sign Up
+            </Button>
+          </>
+        )}
       </div>
     </header>
   );
