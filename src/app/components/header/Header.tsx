512e2bd5669a9aaad9b5c26c967e0090bb436408
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-13 23:26:36 +0900

    ヘッダー修正_2025_0813_23-26

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,69 +1,29 @@
 "use client";
 
-import { useState } from "react";
-
 import useUserStore from "@/store/useUserStore";
-import type { Session } from "@supabase/supabase-js";
-
-import { createClient } from "@/utils/supabase/clients";
 
 import Button from "../button/Button";
 
 export default function Header() {
-  const supabase = createClient();
-
-  const [session, setSession] = useState<Session | null>(null);
-
   const { user } = useUserStore();
-
-  // useEffect(() => {
-  //   supabase.auth.getSession().then(({ data }) => setSession(data.session));
-
-  //   // const { data: loginData } = supabase.auth.onAuthStateChange(
-  //   //   (_event, newSession) => {
-  //   //     setSession(newSession);
-  //   //   },
-  //   // );
-
-  //   // // クリーンアップ
-  //   // return () => {
-  //   //   loginData.subscription.unsubscribe();
-  //   // };
-  // }, [supabase]);
 
   return (
     <header className="w-full">
       <div className="flex h-16 items-center justify-end gap-6 bg-blue-100 pr-10 font-bold">
-        {/* {session ? (
-          <Button
-            // TODO 記事作成ページへ遷移
-            href=""
-            variant="DarkBlue"
-            size="sm"
-          >
-            Create
-          </Button>
-        ) : (
+        {user.id ? (
           <>
-            <Button href="/auth/login" variant="white" size="sm">
-              Login
+            <Button
+              // TODO 記事作成ページへ遷移
+              href=""
+              variant="DarkBlue"
+              size="sm"
+            >
+              Create
             </Button>
-            <Button href="/auth/signup" variant="DarkBlue" size="sm">
-              Sign Up
+            <Button href="/auth/signout/form" variant="DarkBlue" size="sm">
+              Log out
             </Button>
           </>
-          
-        )} */}
-
-        {user ? (
-          <Button
-            // TODO 記事作成ページへ遷移
-            href=""
-            variant="DarkBlue"
-            size="sm"
-          >
-            Create
-          </Button>
         ) : (
           <>
             <Button href="/auth/login" variant="white" size="sm">
