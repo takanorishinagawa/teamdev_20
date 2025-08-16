1cc0aff025415d1130e46d45d219edb3b5790422
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-16 17:47:46 +0900

    追加日_20250816_17-47

--- src/app/components/article/ArticleForm.tsx
+++ src/app/components/article/ArticleForm.tsx
@@ -1,84 +1,135 @@
 "use client";
 
+import { useForm } from "react-hook-form";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
 
 import type { ArticleFormProps } from "@/app/types/types";
+
+import { createClient } from "@/utils/supabase/clients";
 
 import Button from "../button/Button";
 import RectButton from "../button/RectButton";
 
+// 記事投稿フォーム（追加・編集）
+
+type Schema = z.infer<typeof schema>;
+
+const schema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "1文字以上で入力してください。" })
+    .max(20, { message: "20文字以内で入力してください。" }),
+});
+
 const ArticleForm = ({
   type,
-  onSubmit,
   defaultTitle = "",
   defaultContent = "",
   defaultCategory = "",
 }: ArticleFormProps) => {
+  const supabase = createClient();
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { title: "" },
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = () => {};
+
   return (
     <div>
       <div className="p-15">
-        <div className="flex flex-col items-center gap-10">
-          {/* タイトル */}
-          <input
-            type="text"
-            defaultValue={defaultTitle}
-            className="w-full max-w-[1100px] min-w-[600px] text-5xl font-semibold"
-            placeholder="Title"
-          />
+        <form onSubmit={onSubmit}>
+          <div className="flex flex-col items-center gap-10">
+            {/* タイトル */}
+            <input
+              type="text"
+              defaultValue={defaultTitle}
+              className="w-full max-w-[1100px] min-w-[600px] text-5xl font-semibold"
+              placeholder="Title"
+              {...register("title")}
+            />
+            {errors.title && (
+              <p className="mt-1 px-4 text-sm text-red-500">
+                {errors.title.message}
+              </p>
+            )}
 
-          {/* 画像アップロード */}
-          <div className="w-full max-w-[1200px] min-w-[600px] rounded-lg border-2 border-dashed border-[#7777] p-20">
-            <div className="flex flex-col items-center gap-10">
-              {/* 矢印アイコン */}
-              <Image
-                src="/icon/create/arrow.png"
-                alt="アイコン"
-                width={60}
-                height={60}
-              />
+            {/* 画像アップロード */}
+            <div className="w-full max-w-[1200px] min-w-[600px] rounded-lg border-2 border-dashed border-[#7777] p-20">
+              <div className="flex flex-col items-center gap-10">
+                {/* 矢印アイコン */}
+                <Image
+                  src="/icon/create/arrow.png"
+                  alt="アイコン"
+                  width={60}
+                  height={60}
+                />
 
-              {/* アップロードボタン */}
-              <Button
-                variant="Blue"
-                size="lg"
-                onClick={() => alert("画像をアップロード！")}
-              >
-                Upload Image
-              </Button>
+                {/* アップロードボタン */}
+                <Button
+                  variant="Blue"
+                  size="lg"
+                  onClick={() => alert("画像をアップロード！")}
+                >
+                  Upload Image
+                </Button>
+              </div>
+            </div>
+
+            <div className="flex w-full max-w-[1200px] min-w-[600px] justify-end gap-5">
+              {/* 追加日 */}
+              <div className="flex flex-col gap-3">
+                <label htmlFor="add-date">Date Added</label>
+                <input
+                  type="date"
+                  name="add-date"
+                  defaultValue={defaultCategory}
+                  className="w-60 rounded-md border border-[#7777] p-3"
+                ></input>
+              </div>
+
+              {/* カテゴリー */}
+              <div className="flex flex-col gap-3">
+                <label htmlFor="category">Category</label>
+                <select
+                  name="category"
+                  id="category"
+                  defaultValue={defaultCategory}
+                  className="w-60 rounded-md border border-[#7777] p-3"
+                >
+                  <option>Value</option>
+                  <option>Value2</option>
+                  <option>Value3</option>
+                </select>
+              </div>
+            </div>
+
+            {/* 投稿内容 */}
+            <textarea
+              defaultValue={defaultContent}
+              className="min-h-[300px] w-full max-w-[1200px] min-w-[600px] rounded-lg border-none bg-[#D9D9D9]/25 p-5 text-lg text-black"
+              style={{ boxShadow: "2px 2px 10px 0 rgba(0, 0, 0, 0.25)" }}
+            ></textarea>
+
+            {/* 記事作成ボタン */}
+            <div className="flex w-full max-w-[1200px] min-w-[600px] justify-end">
+              <RectButton onClick={onSubmit}>
+                {type === "create" ? "Create" : "Update"}
+              </RectButton>
             </div>
           </div>
-
-          {/* カテゴリー */}
-          <div className="flex w-full max-w-[1200px] min-w-[600px] justify-end">
-            <div className="flex flex-col gap-3">
-              <label htmlFor="category">Category</label>
-              <select
-                name="category"
-                id="category"
-                defaultValue={defaultCategory}
-                className="w-60 rounded-md border border-[#7777] p-3"
-              >
-                <option>Value</option>
-                <option>Value2</option>
-                <option>Value3</option>
-              </select>
-            </div>
-          </div>
-
-          {/* 投稿内容 */}
-          <textarea
-            defaultValue={defaultContent}
-            className="min-h-[300px] w-full max-w-[1200px] min-w-[600px] rounded-lg border-none bg-[#D9D9D9]/25 p-5 text-lg text-black"
-            style={{ boxShadow: "2px 2px 10px 0 rgba(0, 0, 0, 0.25)" }}
-          ></textarea>
-
-          {/* 記事作成ボタン */}
-          <div className="flex w-full max-w-[1200px] min-w-[600px] justify-end">
-            <RectButton onClick={onSubmit}>
-              {type === "create" ? "Create" : "Update"}
-            </RectButton>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
