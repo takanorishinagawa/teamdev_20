a306e59b845597b1e89452ac2083a4bfaceb0196
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-16 17:03:36 +0900

    変更_20250816_17-03

--- src/app/articles/create/page.tsx
+++ src/app/articles/create/page.tsx
@@ -1,14 +1,11 @@
-// 記事投稿用画面
-
 "use client";
 
 import ArticleForm from "@/app/components/article/ArticleForm";
-import React from "react";
+
+// 記事投稿用画面
 
 const CreatePage = () => {
-  return (
-    <ArticleForm type="create" onSubmit={() => alert("投稿！")} />
-  );
+  return <ArticleForm type="create" onSubmit={() => alert("投稿！")} />;
 };
 
 export default CreatePage;
--- src/app/components/article/ArticleForm.tsx
+++ src/app/components/article/ArticleForm.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+
 import Button from "../button/Button";
 import RectButton from "../button/RectButton";
 
 type ArticleFormProps = {
-    type: "create" | "edit";
-    onSubmit: () => void;
-    defaultTitle?: string;
-    defaultContent?: string;
-    defaultCategory?: string;
-
-}
+  type: "create" | "edit";
+  onSubmit: () => void;
+  defaultTitle?: string;
+  defaultContent?: string;
+  defaultCategory?: string;
+};
 
 const ArticleForm = ({
   type,
