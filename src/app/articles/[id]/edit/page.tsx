5aa8e595e858ae561def05adf229c75de570430d
Naoya Noguchi <naoya.work0213@gmail.com>
2025-08-29 18:08:15 +0900

    仮コメントアウト_2025_0829_18-08

--- src/app/articles/[id]/edit/page.tsx
+++ src/app/articles/[id]/edit/page.tsx
@@ -1,26 +1,26 @@
-"use client";
+// "use client";
 
-import ArticleForm from "@/app/components/article/ArticleForm";
+// import ArticleForm from "@/app/components/article/ArticleForm";
 
-// 各記事編集画面用
+// // 各記事編集画面用
 
-const EditArticlePage = () => {
-  const dummyArticle = {
-    title: "既存記事タイトル",
-    content: "ここに既存の本文が入ります",
-    category: "Value2",
-  };
-  return (
-    <div>
-      <ArticleForm
-        type="edit"
-        onSubmit={() => alert("記事を更新！")}
-        defaultTitle={dummyArticle.title}
-        defaultContent={dummyArticle.content}
-        defaultCategory={dummyArticle.category}
-      />
-    </div>
-  );
-};
+// const EditArticlePage = () => {
+//   const dummyArticle = {
+//     title: "既存記事タイトル",
+//     content: "ここに既存の本文が入ります",
+//     category: "Value2",
+//   };
+//   return (
+//     <div>
+//       <ArticleForm
+//         type="edit"
+//         onSubmit={() => alert("記事を更新！")}
+//         defaultTitle={dummyArticle.title}
+//         defaultContent={dummyArticle.content}
+//         defaultCategory={dummyArticle.category}
+//       />
+//     </div>
+//   );
+// };
 
-export default EditArticlePage;
+// export default EditArticlePage;
